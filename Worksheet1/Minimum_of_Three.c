feb306dbe47fb078e405579bbd8bf5c9bf6d5bf7
hanificakmak <36795390+hanificakmak@users.noreply.github.com>
2020-10-18 20:30:00 +0300

    Create Minimum_of_Three.c
    This code is from the Worksheet1. It is asked that "Give two different implementations: (1) use the function TwoMinimum to compute the minimum of three values and (2) write a separate function ThreeMinimum that computes the minimum of three integer values.".

---
+++ Worksheet1/Minimum_of_Three.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+
+int TwoMinimum(int, int);
+int ThreeMinimum(int, int, int);
+
+
+int main(){
+
+	int x,y,z;
+	printf("Enter 3 integers: ");
+	scanf("%d %d %d", &x,&y,&z);
+	
+	if(TwoMinimum(x,y)<TwoMinimum(y,z))
+		printf("Minimum value is %d", TwoMinimum(x,y));
+	
+	else
+		printf("Minimum value is %d", TwoMinimum(y,z));
+	
+return 0;	
+
+}
+
+
+int ThreeMinimum(int x, int y, int z){
+	
+	int min;
+	
+	if(x<y && y<z)
+		return x;
+	
+	else if(y<x && x<z)
+		return y;
+		
+	else 
+		return z;
+	
+}
+
+int TwoMinimum(int x, int y){
+	
+	int min;
+	
+	if(x<y)
+		return x;
+		
+	else 
+		return y;
+	
+}
