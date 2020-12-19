693de44637652104c07ec9c1a5ff596196836a6d
hanificakmak <36795390+hanificakmak@users.noreply.github.com>
2020-12-19 22:03:04 +0300

    Update Q3.c

--- Worksheet8/Q3.c
+++ Worksheet8/Q3.c
@@ -29,58 +29,59 @@ int main(){
 	printf("Enter your birthday: ");
 	scanf("%d/%d/%d", &myself.month, &myself.day, &myself.year);
 
+	
 	if (myself.month==1){
-           printf("You are born in January");
-    }
-     else if (myself.month==2){
-        printf("You are born in February");
-    }
-     else if (myself.month==3){
-        printf("You are born in March");
-    }
-     else if (myself.month==4){
-        printf("You are born in April");
-	}
-     else if (myself.month==5){
-        printf("You are born in May");
-    }
-     else if (myself.month==6){
-        printf("You are born in June");
-	}
-     else if (myself.month==7){
-        printf("You are born in July");
-    }
-     else if (myself.month==8){
-       printf("You are born in August");
-    }
-     else if (myself.month==9){
-        printf("You are born in September");
-    }
-     else if (myself.month==10){
-        printf("You are born in October");
-    }
-     else if (myself.month==11){
-        printf("You are born in November");
-    }
-     else if (myself.month==12){
-        printf("You are born in December");}
+		   printf("You are born in January");
+	    }
+	else if (myself.month==2){
+		printf("You are born in February");
+	    }
+	else if (myself.month==3){
+		printf("You are born in March");
+	    }
+	else if (myself.month==4){
+		printf("You are born in April");
+		}
+	else if (myself.month==5){
+		printf("You are born in May");
+	    }
+	else if (myself.month==6){
+		printf("You are born in June");
+		}
+	else if (myself.month==7){
+		printf("You are born in July");
+	    }
+	else if (myself.month==8){
+	       printf("You are born in August");
+	    }
+	else if (myself.month==9){
+		printf("You are born in September");
+	    }
+	else if (myself.month==10){
+		printf("You are born in October");
+	    }
+	else if (myself.month==11){
+		printf("You are born in November");
+	    }
+	else if (myself.month==12){
+		printf("You are born in December");}
 
-    else
-        printf("Invalid Value!");
+	else
+		printf("Invalid Value!");
     
-    // Q2 Part 
+	// Q2 Part 
 
-    printf("\nYou were born %d days later than 1/1/1900!", days(myself));
-	
-	  // Q3 Part
-	
-    printf("\nEnter date 1: ");
-    scanf("%d/%d/%d", &myself.month, &myself.day, &myself.year);
+	printf("\nYou were born %d days later than 1/1/1900!", days(myself));
 
-    printf("Enter date 2: ");
-    scanf("%d/%d/%d", &other.month, &other.day, &other.year);
+	// Q3 Part
 
-    larger(myself,other);
+	printf("\nEnter date 1: ");
+	scanf("%d/%d/%d", &myself.month, &myself.day, &myself.year);
+
+	printf("Enter date 2: ");
+	scanf("%d/%d/%d", &other.month, &other.day, &other.year);
+
+	larger(myself,other);
 	
 
 return 0;
